608650e885e70a9dc7cce2e1b6ea4af1201f295d
Adam W <126409027+2003nitro@users.noreply.github.com>
2025-02-24 18:24:23 -0500

    basic display for front end

--- IDEAr/src/app/app.component.ts
+++ IDEAr/src/app/app.component.ts
@@ -2,9 +2,12 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
+  styleUrls: ['./app.component.css'],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'IDEAr';
+  Name: string= '';
+  Size: string= '';
+  isMetric: boolean=false;
 }
