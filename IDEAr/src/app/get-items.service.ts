03d07fb32efd46ba8e769ca7bcbaa90ef413e707
N-LaMantia <nickel1097@gmail.com>
2025-02-27 20:26:29 -0500

    Backend stuff v2

--- IDEAr/src/app/get-items.service.ts
+++ IDEAr/src/app/get-items.service.ts
@@ -8,6 +8,32 @@ import { Observable } from 'rxjs';
 export class GetItemsService {
   constructor(private http: HttpClient) {}
 
+//functions requesting data passed from the front end to the backend (ts files). Then, this connects to the API. 
+
+  addItem(name: string, isMetric: boolean, size: string, location: string, threshold: number, isContacted: boolean ): Observable<any> {
+    const url = `http://127.0.0.1:3000/add?name=${encodeURIComponent(name)}&isMetric=${isMetric}&size=${size}&location=${location}&threshold=${threshold}&isContacted=${isContacted}`;
+    return this.http.get(url); 
+  }
+  removeItem(name: string, isMetric: boolean, size: string): Observable<any> {
+    const url = `http://127.0.0.1:3000/remove?name=${encodeURIComponent(name)}&isMetric=${isMetric}&size=${size}`;
+    return this.http.get(url);
+  }
+  incrementItem(name:string, isMetric: boolean, size: string): Observable<any> {
+    const url = `http://127.0.0.1:3000/increment?name=${encodeURIComponent(name)}&isMetric=${isMetric}&size=${size}`;
+    return this.http.get(url);
+
+  }
+  decrementItem(name:string, isMetric: boolean, size: string): Observable<any> {
+    const url = `http://127.0.0.1.3000/decrement?name=${encodeURIComponent(name)}&isMetric=${isMetric}&size=${size}`;
+    return this.http.get(url);
+  }
+
+    
+  findByName(name: string): Observable<any> {
+    const url = `http://127.0.0.1:3000/findByName?name=${encodeURIComponent(name)}`;
+    return this.http.get(url);
+  }
+
   getItem(name: string, isMetric: boolean, size: string): Observable<any> {
     const url = `http://127.0.0.1:3000/find?name=${encodeURIComponent(name)}&isMetric=${isMetric}&size=${size}`;
     return this.http.get(url);
@@ -22,6 +48,7 @@ export class GetItemsService {
   }
   getAllItems(): Observable<any> {
     const url = 'http://127.0.0.1:3000/findAll';
+    
     return this.http.get(url);
   }
 }
