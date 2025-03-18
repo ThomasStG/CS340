131a73fdf9072e91076510eabb7838e2f5290add
Thomas Gallaher <ThomasSGallaher@gmail.com>
2025-03-18 17:44:02 -0400

    updated documentation for python

--- IDEAr/src/app/authentication/authentication.component.ts
+++ IDEAr/src/app/authentication/authentication.component.ts
@@ -10,34 +10,23 @@ import { AuthService } from '../auth.service';
 export class AuthenticationComponent {
   password: string = '';
   username: string = '';
+  cookieName: string = 'auth_token';
 
   constructor(
     private router: Router,
     private authService: AuthService,
   ) {}
-
   login(event: Event): void {
-    console.log('localStorage', typeof localStorage);
-    if (this.username === 't' && this.password === '1') {
-      localStorage.setItem('auth_token', 'test_token');
-      console.log('Login successful');
-      this.router.navigate(['/admin']);
-      return;
-    }
+    console.log(this.username, this.password);
     this.authService.login(this.username, this.password).subscribe({
       next: (response) => {
         if (response.token) {
-          localStorage.setItem('auth_token', response.token);
-          console.log(typeof localStorage);
-          console.log(response.message); // "Login successful"
+          this.router.navigate(['/admin']);
         } else {
           console.error(response.error); // "Invalid credentials"
         }
       },
       error: (err) => console.error('Login request failed', err),
     });
-    this.router.navigate(['/admin']);
   }
-
-  checkPassword(password: string, hash: string): void {}
 }
