63672a8d70600210e48a38c8afb1b2d10193c922
jhenderson2 <jace.henderson@snhu.edu>
2025-04-02 11:20:07 -0400

    guys i got light and dark mode

--- IDEAr/src/app/app.component.ts
+++ IDEAr/src/app/app.component.ts
@@ -1,15 +1,31 @@
 import { Component } from '@angular/core';
 import { AuthService } from './auth.service';
+import {OnInit } from '@angular/core';
+import { UtilityService } from './utility.service';
+import { BehaviorSubject } from 'rxjs';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'IDEAr';
+  darkMode = new BehaviorSubject<boolean>(false);
 
-  constructor(public authService: AuthService) {}
+
+  constructor(public authService: AuthService, public utilityService:UtilityService) {}
   logout() {
     this.authService.logout();
   }
+
+  setMode(val:boolean){
+    this.utilityService.setMode(val);
+    this.darkMode.next(val);
+  }
+
+  ngOnInit(){
+    this.darkMode.next(this.utilityService.isDarkMode());
+  } 
 }
+
