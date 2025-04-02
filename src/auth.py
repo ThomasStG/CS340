ba2f80edb351ffb8d353db7c4751dc14a14ba95d
Thomas Gallaher <ThomasSGallaher@gmail.com>
2025-04-02 11:10:06 -0400

    Played around with adding tables to frontend, did other stuff

--- src/auth.py
+++ src/auth.py
@@ -172,24 +172,23 @@ def get_salt(username: str, cursor: sqlite3.Cursor) -> str:
     return cursor.fetchone()[0]
 
 
-# import hashlib
-# import os
+import hashlib
+import os
 
-# if __name__ == "__main__":
-#     ensure_table()
-#     connection = sqlite3.connect("../data/data.db")
-#     cursor = connection.cursor()
-#     username = "admin"
-#     password = "password"
-#     salt = os.urandom(32)
+if __name__ == "__main__":
+    connection = sqlite3.connect("../data/data.db")
+    cursor = connection.cursor()
+    username = "admin"
+    password = "password"
+    salt = os.urandom(32)
 
-#     # Create a SHA-256 hash object
-#     hash_object = hashlib.sha256()
+    # Create a SHA-256 hash object
+    hash_object = hashlib.sha256()
 
-#     # Update the hash object with the salt and password
-#     hash_object.update(salt + password.encode("utf-8"))
+    # Update the hash object with the salt and password
+    hash_object.update(salt + password.encode("utf-8"))
 
-#     # Get the hashed password as a hexadecimal string
-#     hashed_password = hash_object.hexdigest()
-#     create_account(username, hashed_password, salt.hex(), cursor, connection)
-#     connection.close()
+    # Get the hashed password as a hexadecimal string
+    hashed_password = hash_object.hexdigest()
+    create_account(username, hashed_password, salt.hex(), cursor, connection)
+    connection.close()
