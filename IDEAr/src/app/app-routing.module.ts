fc1fcc13c1b763688486b1f946c8d4ad49bbd34b
jhenderson2 <jace.henderson@snhu.edu>
2025-04-13 18:53:46 -0400

    this one

--- IDEAr/src/app/app-routing.module.ts
+++ IDEAr/src/app/app-routing.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+/*import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [];
@@ -8,3 +8,28 @@ const routes: Routes = [];
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+*/
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { AdminComponent } from './admin/admin.component';
+import { AuthenticationComponent } from './authentication/authentication.component';
+import { DataDownloadComponent } from './data-download/data-download.component';
+import { ElectricalHomeComponent } from './electrical-home/electrical-home.component';
+import { HomeComponent } from './home/home.component';
+import { LogFilePageComponent } from './log-file-page/log-file-page.component';
+
+const routes: Routes = [
+  { path: '', component: HomeComponent }, // Home Page
+  { path: 'admin', component: AdminComponent }, // Admin Page
+  { path: 'authentication', component: AuthenticationComponent },
+  { path: 'data-download', component: DataDownloadComponent },
+  { path: 'logs', component: LogFilePageComponent },
+  { path: 'electrical-home', component: ElectricalHomeComponent },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
