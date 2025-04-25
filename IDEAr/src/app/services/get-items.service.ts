1980837b7ab0005228fa9696cee621b815c2a0ae
jhenderson616 <126726695+jhenderson616@users.noreply.github.com>
2025-04-24 22:21:54 -0400

    Update get-items.service.ts
    comments

--- IDEAr/src/app/services/get-items.service.ts
+++ IDEAr/src/app/services/get-items.service.ts
@@ -6,11 +6,28 @@ import { Observable } from 'rxjs';
   providedIn: 'root',
 })
 export class GetItemsService {
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) {/*
+     * constructor
+     *
+     * Args:
+     *  private http client
+     *
+     * Returns:
+     *   nothin'
+     */}
 
   getItem(name: string, isMetric: boolean, size: string): Observable<any> {
     const url = `http://127.0.0.1:3000/find?name=${encodeURIComponent(name)}&is_metric=${isMetric}&size=${size}`;
     return this.http.get(url);
+    /*
+     * gets item searched for
+     *
+     * Args:
+     *   anme, metric, size
+     *
+     * Returns:
+     *   the specified item
+     */
   }
   getFuzzyItems(
     name: string,
@@ -19,9 +36,27 @@ export class GetItemsService {
   ): Observable<any> {
     const url = `http://127.0.0.1:3000/fuzzyfind?name=${encodeURIComponent(name)}&is_metric=${isMetric}&size=${size}`;
     return this.http.get(url);
+    /*
+     * gets item similar to searched for if it cant get the item
+     *
+     * Args:
+     *   anme, metric, size
+     *
+     * Returns:
+     *   similar item
+     */
   }
   getAllItems(): Observable<any> {
     const url = 'http://127.0.0.1:3000/findAll';
     return this.http.get(url);
+    /*
+     * finds all items
+     *
+     * Args:
+     *   None
+     *
+     * Returns:
+     *   
+     */
   }
 }
