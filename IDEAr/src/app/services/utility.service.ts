9e04d56698d34eba45438b39037e334eeb0d1360
jhenderson616 <126726695+jhenderson616@users.noreply.github.com>
2025-04-24 22:33:49 -0400

    Update utility.service.ts
    comments

--- IDEAr/src/app/services/utility.service.ts
+++ IDEAr/src/app/services/utility.service.ts
@@ -15,17 +15,44 @@ export class UtilityService {
 
   setMode(val: boolean): void {
     this.cookieService.set(this.dayNight, val.toString());
+     /*
+     * sets light or dark mode
+     *
+     * Args:
+     *   boolean
+     *
+     * Returns:
+     *  
+     */
   }
 
   isDarkMode(): boolean {
     const cookie = this.cookieService.get(this.dayNight);
     return cookie.toLowerCase() == 'true';
+     /*
+     * checks to see if this is dark mode
+     *
+     * Args:
+     *   boolean
+     *
+     * Returns:
+     *  true/false
+     */
   }
 
   loadFile(file: string): void {
     this.http
       .get(`http://127.0.0.1:3000/restoreDatabase?file=${file}`)
       .subscribe();
+     /*
+     * loads file
+     *
+     * Args:
+     *   dile as string
+     *
+     * Returns:
+     *  
+     */
   }
   getFiles(): Observable<string[]> {
     // Check if the response is an object and contains the expected data field
@@ -34,6 +61,15 @@ export class UtilityService {
       .pipe(
         map((response) => response.files), // Extract 'data' field from the response
       );
+     /*
+     * gets files
+     *
+     * Args:
+     *   n/a
+     *
+     * Returns:
+     *  returns files
+     */
   }
   backupDatabase(): void {
     this.http.get('http://127.0.0.1:3000/backupDatabase').subscribe({
@@ -45,26 +81,71 @@ export class UtilityService {
         // Optionally, notify the user with an error message
       },
     });
+     /*
+     * backs up database, with error checking if something goes wrong
+     *
+     * Args:
+     *   n/a
+     *
+     * Returns:
+     *  n/a
+     */
   }
   uploadFile(file: any): Observable<any> {
     const formData = new FormData();
     formData.append('file', file);
     return this.http.post('http://127.0.0.1:3000/uploadFile', formData);
+     /*
+     * uploading for files
+     *
+     * Args:
+     *   the file to be uploaded
+     *
+     * Returns:
+     *  the file that has been uploaded
+     */
   }
   appendFile(file: any): Observable<any> {
     const formData = new FormData();
     formData.append('file', file);
     return this.http.post('http://127.0.0.1:3000/appendFile', formData);
+     /*
+     * appends file
+     *
+     * Args:
+     *   file
+     *
+     * Returns:
+     *  appended file
+     */
   }
   downloadFile(fileUrl: string): Observable<Blob> {
     return this.http.get(
       `http://127.0.0.1:3000/downloadFile?fileName=${encodeURIComponent(fileUrl)}`,
       { responseType: 'blob' },
     );
+     /*
+     * downloading a file
+     *
+     * Args:
+     *   the file url as a string
+     *
+     * Returns:
+     *  the file that has been requested (downloaded0
+     */
   }
   getLogFiles(): Observable<string> {
     return this.http.get<{ log: string }>('http://127.0.0.1:3000/get_log').pipe(
       map((response) => response.log), // Extract 'data' field from the response
     );
+     /*
+     * gets the log files
+     *
+     * Args:
+     *   
+     *
+     * Returns:
+     *  list of logs
+     */
   }
 }
